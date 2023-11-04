3cc1ecfc5ea9a98548c2efc3d2914c6eae29c40e
Sarthak Agrawal <sarthavader@gmail.com>
2023-11-04 15:04:40 -0500

    set up event handling

---
+++ chrome-extension/background.js
@@ -0,0 +1,8 @@
+// // background.js
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "openReactPage") {
+//     // chrome.tabs.create({ url: "js/index.html" });
+//     // chrome.action.setPopup({ popup: "js/index.html" });
+//     chrome.sidebarAction.open();
+//   }
+// });
