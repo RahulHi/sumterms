6bee18efea419195458ab0d1401fd1049344d283
RahulHi <67343291+RahulHi@users.noreply.github.com>
2023-11-05 09:35:04 -0600

    JS Convert V2.

--- chrome-extension/background.js
+++
@@ -1,8 +0,0 @@
-// // background.js
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.type === "openReactPage") {
-//     // chrome.tabs.create({ url: "js/index.html" });
-//     // chrome.action.setPopup({ popup: "js/index.html" });
-//     chrome.sidebarAction.open();
-//   }
-// });
--- chrome-extension/content.js
+++
@@ -1,7 +0,0 @@
-// content.js
-const specificElement = true//document.querySelector(".specific-element-selector");
-
-if (specificElement) {
-  // Send a message to the background script
-  chrome.runtime.sendMessage({ type: "Completed Privacy Policy Search", data: false });
-}
