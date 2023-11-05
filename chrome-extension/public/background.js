32e3bfd536d9d9310686b633fb399562323fb221
RahulHi <67343291+RahulHi@users.noreply.github.com>
2023-11-05 10:47:43 -0600

    More message work.

--- chrome-extension/public/background.js
+++ chrome-extension/public/background.js
@@ -6,17 +6,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       let messages = result.messages || [];
 
       // Add the new message to the array
-      messages.push(request.data);
+      messages.push(request);
+
+      console.log(request);
 
       // Save the updated array back to storage
       chrome.storage.local.set({ messages: messages }, function () {
         console.log("Message saved");
       });
 
-      // Retrieve from storage for testing purposes
-      chrome.storage.local.get(["messages"], function (result) {
-        console.log("Value currently is " + result.messages);
-      });
+      // // Retrieve from storage for testing purposes
+      // chrome.storage.local.get(["messages"], function (result) {
+      //   console.log("Value currently is " + result.messages);
+      // });
     });
   }
 });
